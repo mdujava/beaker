mdujava/beaker

commit 70a47505da94770c532e65852587e13ff67bab17

convert server API docs to use Sphinx HTTP domain
Change-Id: I99ed08c20cce7fab9febfbf053f5a10dc7d65bb9

documentation/conf.py
@@ -1,5 +1,6 @@
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo',
+    'sphinxcontrib.httpdomain']
 master_doc = 'index'
 project = u'Beaker'
 copyright = u'2013, Red Hat, Inc'

commit c3e7667fd5195c53aa1bb474c0cfe0d870ecf48a

Add sphinxcontrib-httpdomain as extension
Fixes: #171
Fixes: 70a47505da94 ("convert server API docs to use Sphinx HTTP domain")

beaker.spec
@@ -48,6 +48,7 @@ BuildRequires:  python3-mock
 BuildRequires:  python3-devel
 BuildRequires:  python3-docutils
 BuildRequires:  python3-sphinx
+BuildRequires:  python3-sphinxcontrib-httpdomain
 
 
 %package common

documentation/conf.py
@@ -1,8 +1,8 @@
 import six
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo',
-    'sphinx.ext.extlinks']
+    'sphinx.ext.extlinks', 'sphinxcontrib.httpdomain']
 if six.PY2:
-    extensions += ['sphinxcontrib.httpdomain', 'sphinxcontrib.autohttp.flask']
+    extensions += ['sphinxcontrib.autohttp.flask']
 master_doc = 'index'
 project = u'Beaker'
 copyright = u'2013-2019 Red Hat, Inc.'